4aa88535b5814cddc00af0f11bdea7b7d60e7a63
Nilesh Mali <nilesh@troplabs.com>
2023-04-11 11:05:27 +0530

    feat: add link libs

--- src/builder.ts
+++ src/builder.ts
@@ -135,6 +135,10 @@ export async function buildAndInstallOpenCV(version: string): Promise<void> {
     await saveCache([BUILD_DIR], cacheKey)
   }
   await exec(`sudo make -j${nproc()} -C ${BUILD_DIR} install`)
+  core.exportVariable(
+    'OPENCV_LINK_LIBS',
+    'opencv_highgui,opencv_objdetect,opencv_dnn,opencv_videostab,opencv_calib3d,opencv_features2d,opencv_stitching,opencv_flann,opencv_videoio,opencv_rgbd,opencv_aruco,opencv_video,opencv_ml,opencv_imgcodecs,opencv_imgproc,opencv_core,ittnotify,tbb,liblibwebp,liblibtiff,liblibjpeg-turbo,liblibpng,liblibopenjp2,ippiw,ippicv,liblibprotobuf,quirc,zlib'
+  )
   await exec(`sudo ldconfig`)
   core.endGroup()
 }
