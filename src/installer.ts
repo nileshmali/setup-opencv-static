77414bf032d418c7a034a0f7000d9dac4ff2c407
Nilesh Mali <nilesh@troplabs.com>
2023-05-02 22:49:27 +0530

    feat: add python-numpy package

--- src/installer.ts
+++ src/installer.ts
@@ -6,7 +6,7 @@ export async function installDeps(): Promise<void> {
   core.startGroup('Install prerequisites')
   if (sys.isLinux()) {
     await exec('sudo apt-get update')
-    await exec('sudo apt-get install -y clang build-essential cmake nasm')
+    await exec('sudo apt-get install -y clang build-essential cmake nasm python-numpy')
   } else {
     throw new Error(`Unsupported platform '${sys.platform()}'`)
   }
