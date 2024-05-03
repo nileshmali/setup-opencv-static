7523fff9a0548ec5ad4f2a1c32d0cd25e67aea37
Nilesh Mali <nilesh@troplabs.com>
2024-05-03 17:01:08 +0530

    feat: run build

--- dist/index.js
+++ dist/index.js
@@ -169,7 +169,7 @@ async function buildAndInstallOpenCV(version) {
         core.endGroup();
     }
     core.startGroup('Install OpenCV');
-    await (0, exec_1.exec)(`sudo make -j${(0, system_1.nproc)()} -C ${constants_1.BuildDir} install`);
+    await (0, exec_1.exec)(`make -j${(0, system_1.nproc)()} -C ${constants_1.BuildDir} install`);
     core.exportVariable('OPENCV_LINK_LIBS', 'opencv_highgui,opencv_objdetect,opencv_dnn,opencv_calib3d,opencv_features2d,opencv_stitching,opencv_flann,opencv_videoio,opencv_video,opencv_ml,opencv_imgcodecs,opencv_imgproc,opencv_core,libittnotify,libtbb,liblibwebp,liblibtiff,liblibjpeg-turbo,liblibpng,liblibopenjp2,libippiw,libippicv,liblibprotobuf,libquirc,libzlib');
     core.endGroup();
 }
